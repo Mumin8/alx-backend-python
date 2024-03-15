661dbfec4d39013c5f9242826cffae5b93379e35
Mumin8 <alhassanmumin8@gmail.com>
2024-03-15 23:31:09 +0000

    add floats

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''this will add two floats and return a float result'''
+
+def add(a: float, b: float) -> float:
+    '''
+    add this takes two values and add them
+    '''
+
+    return a + b
