fac0683b19bbae58b5ac87d846585aa9224d6c32
Mumin8 <alhassanmumin8@gmail.com>
2024-03-15 23:46:33 +0000

    concatenate strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''this will concatenate strings'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''
+    concat:
+        it will concatenate str1 and str2 and return str
+    '''
+    
+    return str1 + str2
