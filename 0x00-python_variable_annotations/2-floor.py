3b07620e8b72d0aa4327617f6c8618a7a83b5d3d
Mumin8 <alhassanmumin8@gmail.com>
2024-03-15 23:53:15 +0000

    floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import math
+'''this will floor a float'''
+
+
+def floor(n: float) -> int:
+    '''
+    floor:
+        it will floor n and return the integer
+    '''
+
+    return math.floor(n)
