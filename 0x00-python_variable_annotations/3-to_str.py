60d3cf7f765cb9715ca2c75f2c953f9316f9042e
Mumin8 <alhassanmumin8@gmail.com>
2024-03-16 09:25:09 +0000

    annotation

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''this will cast a float to string'''
+
+
+def to_str(n: float) -> str:
+    '''
+    to_str:
+        it will cast a float to a string and return the string
+    '''
+
+    return str(n)
