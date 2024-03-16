08236c33bab9d9a3b1054596f753995781a477d3
Mumin8 <alhassanmumin8@gmail.com>
2024-03-16 10:07:23 +0000

    annotation

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -5,13 +5,13 @@ from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-  """Creates a tuple with a string key and the square of a numeric value.
+    """Creates a tuple with a string key and the square of a numeric value.
 
-  Args:
-      k: A string key.
-      v: An integer or float value.
+    Args:
+        k: A string key.
+        v: An integer or float value.
 
-  Returns:
-      A tuple containing the key (string) and the square of the value (float).
-  """
-  return k, float(v ** 2)
+    Returns:
+        A tuple containing the key (string) and the square of the value (float).
+    """
+    return k, float(v ** 2)
