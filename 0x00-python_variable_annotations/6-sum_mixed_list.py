3f459367f3cc0ac42b05a02aea74ef9d47b3139e
Mumin8 <alhassanmumin8@gmail.com>
2024-03-16 09:56:52 +0000

    sum mixed list

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -2,10 +2,10 @@
 '''this will floor a float'''
 
 
-from typing import Union
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: list[Union[int, float]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """Sums a list of integers and floats.
 
     Args:
