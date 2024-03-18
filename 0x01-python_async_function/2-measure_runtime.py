aadec50b6728a617fa9abac80864342eb49626dd
Mumin8 <alhassanmumin8@gmail.com>
2024-03-18 22:46:17 +0000

    async

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+'''Basic async module'''
+
+
+import asyncio
+import time
+from typing import List
+
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    '''this function will wait for a random time and return it'''
+
+    start_time = time.time()
+    await wait_n(n, max_delay)
+    end_time = time.time()
+    total_time = end_time - start_time
+    return total_time / n
